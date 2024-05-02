ea9a1227c26edff3ec960578305c0efe53f91116
Hari Krishnan VC <133744428+harivc69@users.noreply.github.com>
2024-05-02 11:12:00 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,1 +1,14 @@
-console.log("Welcome to AWS");
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample React app.</p>
+    </div>
+  );
+}
+
+export default App;
+
